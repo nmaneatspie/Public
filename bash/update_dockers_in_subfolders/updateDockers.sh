1d37e655aa40adb913d7c9263a65630f23741d61
Norman <64245789+nmaneatspie@users.noreply.github.com>
2026-05-18 13:23:29 +1000

    Create updateDockers.sh

---
+++ bash/update_dockers_in_subfolders/updateDockers.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#change base_dir to folder where the docker folders are held
+base_dir=/home/docker/dockers/
+
+#read -p "Do you want to run 'docker compose up -d' as well? y/[n] " run
+#run=${run:-"n"}
+run="y"
+
+docker_update(){
+  cd $2
+  echo "Updating docker at" $2
+
+  if [ $run == "y" ]
+  then
+    docker compose pull; docker compose up -d
+  else
+    docker compose pull
+  fi
+
+  cd $1
+}
+
+for folder in $base_dir/*
+do
+  for file in $folder/*
+  do
+    filename=$(basename $file)
+    if [ $filename == "compose.yml" ] || [ $filename == "compose.yaml" ]
+    then
+      docker_update $base_dir $folder; break
+    elif [ $filename == "docker-compose.yml" ] || [ $filename == "docker-compose.yaml" ]
+    then
+      docker_update $base_dir $folder; break
+    fi
+
+  done
+done
