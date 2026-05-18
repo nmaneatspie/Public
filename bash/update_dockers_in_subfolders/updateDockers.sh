b02a9b17d9240cbe1980f566dca97c9389bb585d
Norman <64245789+nmaneatspie@users.noreply.github.com>
2026-05-18 13:50:28 +1000

    Update updateDockers.sh

--- bash/update_dockers_in_subfolders/updateDockers.sh
+++ bash/update_dockers_in_subfolders/updateDockers.sh
@@ -9,29 +9,29 @@ run="y"
 docker_update(){
   cd $2
   echo "Updating docker at" $2
-
   if [ $run == "y" ]
   then
     docker compose pull; docker compose up -d
   else
     docker compose pull
   fi
-
   cd $1
 }
 
 for folder in $base_dir/*
 do
-  for file in $folder/*
-  do
-    filename=$(basename $file)
-    if [ $filename == "compose.yml" ] || [ $filename == "compose.yaml" ]
-    then
-      docker_update $base_dir $folder; break
-    elif [ $filename == "docker-compose.yml" ] || [ $filename == "docker-compose.yaml" ]
-    then
-      docker_update $base_dir $folder; break
-    fi
-
-  done
+  foldername=$(basename $folder)
+  if [ $foldername != "ignore1" ] && [ $foldername != "ignore2" ]
+    for file in $folder/*
+    do
+      filename=$(basename $file)
+      if [ $filename == "compose.yml" ] || [ $filename == "compose.yaml" ]
+      then
+        docker_update $base_dir $folder; break
+      elif [ $filename == "docker-compose.yml" ] || [ $filename == "docker-compose.yaml" ]
+      then
+        docker_update $base_dir $folder; break
+      fi
+    done
+  fi
 done
